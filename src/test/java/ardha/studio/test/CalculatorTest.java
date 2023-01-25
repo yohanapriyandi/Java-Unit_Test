42a8857e23050666c5d135ff00a8ddb88a16383c
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-01-26 05:38:12 +0700

    using class SimpleDisplayNameGenerator

--- src/test/java/ardha/studio/test/CalculatorTest.java
+++ src/test/java/ardha/studio/test/CalculatorTest.java
@@ -1,25 +1,33 @@
 package ardha.studio.test;
 
+import ardha.studio.test.generator.SimpleDisplayNameGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(SimpleDisplayNameGenerator.class)
+//@DisplayName("Test untuk calculator app")
 public class CalculatorTest {
 
     private Calculator calculator = new Calculator();
 
     @Test
+    //@DisplayName("Test skenario sukses untuk method add()")
     public  void testAddSuccess(){
         var result = calculator.add(15,56);
         assertEquals(71, result);
     }
 
     @Test
+    //@DisplayName("Test skenario suskes untuk method divide()")
     public void testDivideSuccess(){
         var result = calculator.divide(100, 10);
         assertEquals(10, result);
     }
 
     @Test
+    //@DisplayName("Test skenario failed untuk method divide()")
     public void testDivideFailed() {
         assertThrows(IllegalArgumentException.class, () -> {
             calculator.divide(10, 0);
