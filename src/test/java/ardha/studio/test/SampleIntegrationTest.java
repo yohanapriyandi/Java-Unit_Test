bb13e8130214f3b70956cfb60ffe60aa2089d858
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-01-29 09:37:18 +0700

    Penggunaan Tag di unit test menggunakan Annotation @Tags dan @Tag

---
+++ src/test/java/ardha/studio/test/SampleIntegrationTest.java
@@ -0,0 +1,20 @@
+package ardha.studio.test;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+@Tags({
+        @Tag("integration-test")
+})
+public class SampleIntegrationTest {
+    @Test
+    void test1() {
+
+    }
+
+    @Test
+    void test2() {
+
+    }
+}
