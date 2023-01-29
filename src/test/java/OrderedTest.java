8860cd7791e227fb0917c4e7d78da8fb22ba800a
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-01-29 10:05:53 +0700

    Implementasi lifecyclel PER_CLASS dan PER_METHOD

--- src/test/java/OrderedTest.java
+++ src/test/java/OrderedTest.java
@@ -1,24 +1,28 @@
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderedTest {
+
+    private  int counter =   0;
+
     @Test
     @Order(3)
     void test1() {
-
+        counter++;
+        System.out.println(counter);
     }
     @Test
     @Order(1)
     void test3() {
-
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(2)
     void test2() {
-
+        counter++;
+        System.out.println(counter);
     }
 }
