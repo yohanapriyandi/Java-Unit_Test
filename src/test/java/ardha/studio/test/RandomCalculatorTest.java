cbc15c43f4fe4871b9ba06d779c96c8968072037
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-01-30 21:52:48 +0700

    mengulangi test menggunakan RepeatedTest Anotation

--- src/test/java/ardha/studio/test/RandomCalculatorTest.java
+++ src/test/java/ardha/studio/test/RandomCalculatorTest.java
@@ -2,6 +2,7 @@ package ardha.studio.test;
 
 import ardha.studio.test.resolver.RandkomParameterResolver;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extensions;
@@ -9,7 +10,22 @@ import org.junit.jupiter.api.extension.Extensions;
 import java.util.Random;
 
 public class RandomCalculatorTest extends ParentCalculatorTest{
+    @Test
      void testRandom(Random random){
+        var a = random.nextInt();
+        var b = random.nextInt();
+
+        var result = calculator.add(a,b);
+        var expected = a + b;
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @RepeatedTest(
+            value = 10,
+            name = "{displayName} ke {currentRepetition} dari {totalRepetitions}"
+    )
+    void testRandomRepeat(Random random){
         var a = random.nextInt();
         var b = random.nextInt();
 
