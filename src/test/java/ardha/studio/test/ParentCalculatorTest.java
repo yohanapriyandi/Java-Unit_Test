bb80fc7548e02c011d7d5ddd04a9c6530bc7e859
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-01-30 21:52:06 +0700

    menambahkan setup method

--- src/test/java/ardha/studio/test/ParentCalculatorTest.java
+++ src/test/java/ardha/studio/test/ParentCalculatorTest.java
@@ -1,6 +1,7 @@
 package ardha.studio.test;
 
 import ardha.studio.test.resolver.RandkomParameterResolver;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extensions;
@@ -10,4 +11,9 @@ import org.junit.jupiter.api.extension.Extensions;
 })
 public class ParentCalculatorTest {
     protected Calculator calculator = new Calculator();
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Before Each");
+    }
 }
