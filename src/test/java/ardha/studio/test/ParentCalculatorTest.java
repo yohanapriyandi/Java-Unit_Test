54fa52f8afa54c38072cc6428e42710390724f57
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-01-30 21:36:08 +0700

    pewarisan di unit test

---
+++ src/test/java/ardha/studio/test/ParentCalculatorTest.java
@@ -0,0 +1,13 @@
+package ardha.studio.test;
+
+import ardha.studio.test.resolver.RandkomParameterResolver;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extensions;
+
+@Extensions({
+        @ExtendWith(RandkomParameterResolver.class)
+})
+public class ParentCalculatorTest {
+    protected Calculator calculator = new Calculator();
+}
