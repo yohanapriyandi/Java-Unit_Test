3f521f2c567ed70176f8265f1c0b510bf1055657
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-02-01 11:31:24 +0700

    Add Annotation @DisplayName

--- src/test/java/ardha/studio/test/RandomCalculatorTest.java
+++ src/test/java/ardha/studio/test/RandomCalculatorTest.java
@@ -2,6 +2,7 @@ package ardha.studio.test;
 
 import ardha.studio.test.resolver.RandkomParameterResolver;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,6 +22,7 @@ public class RandomCalculatorTest extends ParentCalculatorTest{
         Assertions.assertEquals(expected, result);
     }
 
+    @DisplayName("Test random calculator")
     @RepeatedTest(
             value = 10,
             name = "{displayName} ke {currentRepetition} dari {totalRepetitions}"
